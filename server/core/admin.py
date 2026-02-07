65fc3275e8dedcf7843ce1e99130852f01eea6dd
vansh-bit <sharmavansh197900@gmail.com>
2026-02-07 16:37:24 +0530

    Added Django models and initial migration

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import (
+    User, 
+    Ingredient, 
+    Tag, 
+    SearchHistory, 
+    Recipe, 
+    RecipeIngredient, 
+    RecipeTag, 
+    UserPantry, 
+    Review
+)
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Ingredient)
+admin.site.register(Tag)
+admin.site.register(SearchHistory)
+admin.site.register(Recipe)
+admin.site.register(RecipeIngredient)
+admin.site.register(RecipeTag)
+admin.site.register(UserPantry)
+admin.site.register(Review)
